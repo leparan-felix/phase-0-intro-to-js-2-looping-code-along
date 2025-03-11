8f2132fa1d4a0fd80bd857ee0e100a5d9b7fc26d
leparan <leparanfelix16@gmail.com>
2025-03-11 12:52:00 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let messages = []; // Create an empty array to hold messages
+  
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+  
+    return messages;
+  }
+  
+  // Example call
+  console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+  
+  function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--; // Decrement num after each iteration
+    }
+  }
+  
+  // Example call
+  countDown(10);
+  
